cca8a79804bf16d48a998afe85fe0a24a0d2f8df
mike trujillo <38016591+mike121986@users.noreply.github.com>
2021-08-03 23:36:11 -0500

    primerios pasos en vuejs
    ahoira comprendemos por que son las cosas

---
+++ hola mundo/components/Footer.js
@@ -0,0 +1,7 @@
+app.component('footer-banco', {
+    template: /*html*/`
+        <div class="bg-dark py-3 mt-2 text-white text-center">
+            <h3>Footer de mi pagina web</h3>
+        </div>
+    `
+})
---
+++ hola mundo/main.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            titulo: 'Mi banco con Vuejs',
+            cantidad: 1000,
+            enlace: 'http://ticdwem.com',
+            estado: false,
+            servicios:['transferencias','pagos','servicios','cheques'],
+            desactivar: false
+        }
+    },
+    // para trabajar con los eventos como el click es necesario trabajar con metodos
+    // este es un objeto con diferente funcion
+    methods: {
+        agregarSaldo(){
+            this.cantidad = this.cantidad +100
+        },
+        disminuirSaldo(valor){
+            if(this.cantidad === 0 ){
+                this.desactivar = true
+                alert('Saldo en cero')
+                return
+            }
+            this.cantidad = this.cantidad - valor
+        }
+    },
+
+    computed:{
+        colorCantidad(){
+            return this.cantidad>500?' text-success':' text-danger'
+        },
+        mayusculasTExto(){
+            return this.titulo.toUpperCase()
+        }
+    }
+})
